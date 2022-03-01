6edf6f04e7d9dd9e2148afc4e40d24cddacd06f9
tiagoanatar <35281635+tiagoanatar@users.noreply.github.com>
2022-02-28 23:46:36 -0300

    Add files via upload

---
+++ src/components/SetupScriptComputed/SetupScriptComputed.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="parent-child">
+    <div class="parent-child-internal">
+      <h3>Computed Sample</h3>
+      Made to manipulate multiple props/data in a function
+      {{ classIconName }}<br />
+      {{ props.msg }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, computed } from 'vue';
+
+interface Props {
+  msg?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  msg: 'test',
+});
+
+const classIconName = computed<string>(() => `theme-icon-${props.msg}`);
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  ul li {
+    list-style: none;
+    border-bottom: 1px solid #000;
+    padding: 10px;
+  }
+</style>
