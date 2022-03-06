0979e2506d57bbb23952513224654f9168527ecf
tiagoanatar <35281635+tiagoanatar@users.noreply.github.com>
2022-03-06 09:30:01 -0300

    Add files via upload

--- src/components/SetupScriptRadioWatch/RadioWatch.vue
+++ src/components/SetupScriptRadioWatch/RadioWatch.vue
@@ -1,5 +1,12 @@
 <template>
-  <div :class="isChecked.value ? 'form-check radio checked' : 'form-check radio'" @click="clickHandler">
+  <div
+    :class="cxRadio"
+    @click="clickHandler"
+  >
+  <!--<div
+    :class="isChecked.value ? 'form-check radio checked' : 'form-check radio'"
+    @click="clickHandler"
+  >-->
     <input
       :id="inputId"
       class="form-check-input"
@@ -18,7 +25,7 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, watch } from 'vue';
+import { reactive, watch, watchEffect, computed } from "vue";
 
 // TODO: create data-automation id
 interface Props {
@@ -32,44 +39,63 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  checkedValue: '',
-  describedBy: 'Default describedby',
+  checkedValue: "",
+  describedBy: "Default describedby",
   disabled: false,
-  inputId: 'Default1D',
-  inputName: 'Default input name',
-  hasOptional: '',
-  label: 'Default label',
+  inputId: "Default1D",
+  inputName: "Default input name",
+  hasOptional: "",
+  label: "Default label",
 });
 
-const emit = defineEmits(['click']);
+const emit = defineEmits(["click"]);
 
 const isChecked = reactive({ value: false });
 
 function clickHandler() {
-  emit('click', props.value);
-  //isChecked.value = !isChecked.value;
-  console.log(isChecked.value);
+  emit("click", props.value);
+  //console.log(isChecked.value);
 }
+~
+/*
+watch(
+  () => props.checkedValue,
+  (selection, old) => {
+    if (selection === props.value) {
+      isChecked.value = true;
+    } else {
+      isChecked.value = false;
+    }
+  }
+);
+*/
 
-watch(() => props.checkedValue, (selection) => { 
-  if (selection === props.value) {
+
+watchEffect(() => {
+  if (props.checkedValue === props.value) {
     isChecked.value = true;
   } else {
     isChecked.value = false;
   }
 })
 
+
+const cxRadio = computed(() => [
+  "form-check",
+  "radio",
+  isChecked.value && "checked",
+]);
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-  ul li {
-    list-style: none;
-    border-bottom: 1px solid #000;
-    padding: 10px;
-  }
+ul {
+  margin: 0;
+  padding: 0;
+}
+ul li {
+  list-style: none;
+  border-bottom: 1px solid #000;
+  padding: 10px;
+}
 </style>
